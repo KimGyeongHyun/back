e8d73e3c366c9019da1da188d443b4a97564c39c
KimGyeongHyun <105264179+KimGyeongHyun@users.noreply.github.com>
2023-01-21 23:44:04 +0900

    23-01-21(2)

---
+++ 23-01-21/ex_10952(2).py
@@ -0,0 +1,48 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def __str__(self):
+
+        temp_string = ""
+        iterator = self.head
+
+        while iterator is not None:
+            temp_string += "{}\n".format(iterator.data)
+            iterator = iterator.next
+
+        return temp_string
+
+
+if __name__ == "__main__":
+
+    linked_list = LinkedList()
+
+    while True:
+        a, b = map(int, input().split())
+
+        if a == 0 and b == 0:
+            break
+
+        linked_list.append(a + b)
+
+    print(linked_list)
---
+++ 23-01-21/ex_11021(2).py
@@ -0,0 +1,48 @@
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def append(self, data):
+
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def __str__(self):
+
+        temp_string = ""
+        iterator = self.head
+        case_number = 1
+
+        while iterator is not None:
+            temp_string += "Case #{}: {}\n".format(case_number, iterator.data)
+            iterator = iterator.next
+            case_number += 1
+
+        return temp_string
+
+
+if __name__ == "__main__":
+
+    linked_list = LinkedList()
+
+    repeat = int(input())
+
+    for i in range(repeat):
+        a, b = map(int, input().split())
+        linked_list.append(a + b)
+
+    print(linked_list)

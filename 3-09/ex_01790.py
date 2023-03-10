942de55fa917c1370902479150fbca7a779aad3f
KimGyeongHyun <potter9602@naver.com>
2023-03-10 19:28:04 +0900

    add comments

--- 3-09/ex_01790.py
+++ 3-09/ex_01790.py
@@ -2,25 +2,25 @@ from sys import stdin
 
 
 def get_kth_number(n, k):
-    div = 1
-    exp = 1
+    div = 1     # 10 ** (n의 자릿수)
+    exp = 1     # 자릿수에 따른 숫자 갯수 (2자리면 2, 3자리면 3 ... )
 
-    while n//div != 0:
+    while n//div != 0:  # div *= 10 연산 계속하여 n의 끝까지 갈 때까지 반복
 
-        if 9 * div * exp < k:
-            k -= 9 * div * exp
+        if 9 * div * exp < k:   # 찾아야 하는 k 가 현재 자릿수의 총 숫자 갯수보다 많으면
+            k -= 9 * div * exp  # k 에서 현재 자릿수의 총 숫자만큼 뺄셈
             div *= 10
-            exp += 1
+            exp += 1            # 자릿수 옮김
 
-        else:
-            if (n - div + 1) * exp < k:
+        else:   # k가 현재 자릿수의 총 숫자 갯수 안에 있다면 k번째 숫자를 찾아 리턴
+            if (n - div + 1) * exp < k:     # k가 n 에서 최대 출력할 수 있는 숫자 개수를 초과했다면 탈출
                 break
 
-            th1 = (k-1) // exp + div
+            th1 = (k-1) // exp + div    # k번째 자리에 있는 숫자는 무슨 숫자인지 (ex: 15, 385, 2934 ...)
             th2 = (k-1) % exp
-            th2 = exp - th2 - 1
+            th2 = exp - th2 - 1         # th1 에서 몇번째 자리인지
 
-            return th1//(10**th2) % 10
+            return th1//(10**th2) % 10  # 숫자 th1 에서 th2 번째 자릿수 반환
 
     return -1
 
--- 3-09/ex_02022.py
+++ 3-09/ex_02022.py
@@ -3,23 +3,33 @@ from sys import stdin
 
 def get_guess_width(x, y, c):
 
+    # width 를 유추해 c를 계산
+    # 계산한 c와 파라미터 c를 비교하여 이분법으로 width 를 다시 설정
+    # 계산한 c가 파라미터 c의 특정 범위 내에 들어갔다면 width 리턴
+
+    # width 의 max 값은 x, y 를 넘을 수 없음
+    # 즉 x, y중 작은 값을 max 로 설정
     if x < y:
         max = x
     else:
         max = y
 
+    # max와 min 사이값을 width 로 설정
     width = max/2
     min = 0
 
     while True:
+
+        # width 로 guess_c 를 구함
         left = (x ** 2 - width ** 2) ** (1 / 2)
         right = (y ** 2 - width ** 2) ** (1 / 2)
-
         guess_c = left * right / (left + right)
 
+        # c와 유사하다면 리턴
         if -0.0005 <= guess_c - c <= 0.0005:
             return width
 
+        # c와 값이 차이가 난다면 max or min 값을 이분법으로 조정해 다시 width 를 설정
         if guess_c < c:
             max -= (max - min) / 2
         else:

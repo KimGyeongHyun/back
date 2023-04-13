a0492c638e81fc2690b86e232fd73470f8faa8c9
KimGyeongHyun <potter9602@naver.com>
2023-04-13 12:18:28 +0900

    Update ex_01929.py
    소수 판별 에라토스테네스의 체 추가

--- prime/ex_01929.py
+++ prime/ex_01929.py
@@ -16,6 +16,18 @@ def is_prime(n):
     return True
 
 
+def set_prime_eratos(n):
+    a = [True for _ in range(n+2)]
+    a[0] = False
+    a[1] = False
+    for i in range(2, int(n**(1/2)) + 1):
+        if a[i] is True:
+            for j in range(2 * i, n+2, i):
+                a[j] = False
+
+    return a
+
+
 if __name__ == "__main__":
     m, n = map(int, stdin.readline().split())
 
